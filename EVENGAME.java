1da2a926419bd6d1641d0de78d127044ff8a4711
ARJUN WANKHEDE <66715308+Arjun2905@users.noreply.github.com>
2021-02-03 21:31:11 +0530

    EVENGAME-codechef solution

---
+++ EVENGAME.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try {
+    	    Scanner sc = new Scanner(System.in);
+    	    int t = sc.nextInt();
+    	    while(t-- > 0){
+    	        int n = sc.nextInt();
+    	        int[] arr = new int[n];
+    	        int sum = 0;
+    	        for(int i=0;i<n;i++){
+    	            arr[i] = sc.nextInt();
+    	            sum += arr[i];
+    	        }
+    	        
+    	        if(sum%2==0){
+    	            System.out.println(1);
+    	        }else{
+    	            System.out.println(2);
+    	        }
+    	    }
+	    } catch(Exception e) {
+	    }
+		// your code goes here
+	}
+}
